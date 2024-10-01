11529a717fc135bebde1d8c671a60cd201cae6ad
chuma-direktor <altaichuma04@gmail.com>
2024-10-01 13:15:54 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+number_of_completed_tasks = 12
+number_of_hours_spent = 1.5
+course_name = "Python"
+video_lesson_viewing_time = 0.15
+time_for_solving_practice = 0.20
+total_time = 0.35
+print("Название курса" , course_name, "Количество выполненныз ДЗ", number_of_completed_tasks,
+      "Кол-во  затраченных часов", total_time, "часа" )
